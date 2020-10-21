9d3c144795a305cc0430a55976af69e50d75e563
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-21 20:39:21 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,71 @@
-# Add your code here
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list(songs)
+  counter = 0 
+  songs.each_with_index do |song, index|
+    puts "#{index += 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  
+  if (1..9).to_a.index(input.to_i) != nil 
+    puts "Playing #{songs[input.to_i - 1]}"
+  elsif songs.index(input) != nil 
+    puts "Playing #{input}"
+  else 
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  puts "Please enter a command:"
+  input = gets.strip
+  
+  while input != "exit"
+    if input == "list"
+      list(songs)
+      inputs = gets.strip
+      break
+    elsif input == "play"
+      play(songs)
+      inputs = gets.strip
+      break
+    elsif input == "help"
+      help 
+      inputs = gets.strip
+      break 
+    else 
+      puts "invalid command"
+      help
+      inputs = gets.strip
+      break
+    end
+  end
+  exit_jukebox
+end
